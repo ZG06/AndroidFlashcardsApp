ef8a851387327a7fd0bee5492e7645b69a8c811a
ZG06 <s1demusic06@gmail.com>
2025-06-22 22:29:38 +0200

    Add home page UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,58 @@
-import {Text, View} from "react-native";
+import {ScrollView, Text, TouchableOpacity, View} from "react-native";
 import React from "react";
+import * as Progress from 'react-native-progress';
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function Index() {
-
     return (
-        <View>
-            <Text>
-                Home
-            </Text>
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false} className={"p-6"}>
+            <View className={"bg-white p-6 rounded-md shadow-sm shadow-gray-200 mb-6"}>
+                <Text className={"font-bold text-2xl mb-2"}>Today&#39;s Progress</Text>
+                <View className={"flex-row justify-center gap-x-20 mb-5"}>
+                    <View className={"items-center"}>
+                        <Text className={"text-blue-600 font-bold text-xl"}>47</Text>
+                        <Text className={"text-gray-500"}>Cards Studied</Text>
+                    </View>
+                    <View className={"items-center"}>
+                        <Text className={"text-green-600 font-bold text-xl"}>32m</Text>
+                        <Text className={"text-gray-500"}>Time Spent</Text>
+                    </View>
+                </View>
+                <View>
+                    <View className={"flex-row justify-between mb-2"}>
+                        <Text>Daily Goal</Text>
+                        <Text>47/50 cards</Text>
+                    </View>
+                    <View className={"w-full"}>
+                        <Progress.Bar progress={(47/50)} color={"black"} width={null} borderColor={"white"} unfilledColor={"#f3f4f6"} height={7} />
+                    </View>
+                </View>
+            </View>
+
+            <View className={"flex-row gap-x-5 mb-6"}>
+                <TouchableOpacity className={"flex-1 bg-white p-6 rounded-md shadow-xl shadow-gray-200 items-center hover:shadow-md"}>
+                    <MaterialIcons name={"play-arrow"} color={"#2563eb"} size={38} />
+                    <Text className={"font-bold text-lg"}>Quick Study</Text>
+                    <Text className={"text-gray-500"}>Continue learning</Text>
+                </TouchableOpacity>
+                <TouchableOpacity className={"flex-1 bg-white p-6 rounded-md shadow-xl shadow-gray-200 items-center hover:shadow-md"}>
+                    <MaterialIcons name={"add"} color={'#16a34a'} size={38} />
+                    <Text className={"font-bold text-lg"}>Create deck</Text>
+                    <Text className={"text-gray-500"}>Add new cards</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View>
+                <View className={"flex-row justify-between mb-20"}>
+                    <Text className={"font-bold text-xl"}>Recent Decks</Text>
+                    <TouchableOpacity className={"h-7 w-20 hover:bg-gray-200 hover:rounded-md items-center"}>
+                        <Text className={"text-[16px]"}>View All</Text>
+                    </TouchableOpacity>
+                </View>
+                <View className={"justify-center items-center"}>
+                    <Text className={"text-gray-500"}>No recent decks.</Text>
+                </View>
+            </View>
+        </ScrollView>
     );
 }
