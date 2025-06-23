a3565e5220e0ae646090d2a0fc8005346111a051
ZG06 <s1demusic06@gmail.com>
2025-06-23 20:12:58 +0200

    Add decks screen UI

--- app/(tabs)/decks.tsx
+++ app/(tabs)/decks.tsx
@@ -1,13 +1,30 @@
-import {Text, View} from "react-native";
+import {Text, TouchableOpacity, View} from "react-native";
 import React from "react";
+import SearchBar from "@/components/SearchBar";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
-export default function Decks() {
+export default function Deckws() {
 
     return (
         <View>
-            <Text>
-                Decks
-            </Text>
+            {
+                // Header
+            }
+            <View className={"flex justify-center items-start h-[160px] px-6 bg-white"}>
+                <View className={"flex-row justify-between w-full py-2"}>
+                    <Text className={"text-[24px] font-bold mb-4"}>My Decks</Text>
+                    <TouchableOpacity
+                        className={"flex-row items-center justify-center gap-x-2 bg-black h-9 w-[110px] rounded-md px-3"}
+                    >
+                        <MaterialIcons name={"add"} color={"white"} size={15}/>
+                        <Text className={"text-white font-semibold text-[13px]"}>New Deck</Text>
+                    </TouchableOpacity>
+                </View>
+                <SearchBar />
+            </View>
+            <View className={"justify-center items-center mt-20"}>
+                <Text className={"text-gray-500"}>No decks found.</Text>
+            </View>
         </View>
     );
 }
