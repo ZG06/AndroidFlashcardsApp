47b6f416c88bd4eddbb6651766f0168bbe8103c1
ZG06 <s1demusic06@gmail.com>
2025-06-24 13:01:05 +0200

    Implement add card and delete card functionality on new deck screen

--- components/NewFlashcard.tsx
+++ components/NewFlashcard.tsx
@@ -2,18 +2,42 @@ import {Text, TextInput, TouchableOpacity, View} from "react-native";
 import React, {useState} from "react";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
-export default function NewFlashcard() {
+export default function NewFlashcard({
+    index = 1,
+    onDelete,
+    canDelete
+}: {
+    index?: number;
+    onDelete?: () => void;
+    canDelete: boolean;
+}) {
     const [frontValue, setFrontValue] = useState('');
     const [backValue, setBackValue] = useState('');
 
     return (
         <View className={"bg-white p-6 rounded-md shadow-sm shadow-gray-200 mb-6"}>
             <View className={"flex-row justify-between"}>
-                <Text className={"font-semibold text-[16px] mb-5"}>Card 1</Text>
-                <TouchableOpacity className={"flex-row gap-x-1 hover:bg-gray-100 hover:rounded-md h-6 w-20 justify-center"}>
-                    <MaterialIcons name="visibility" size={18} color="black" />
-                    <Text className={"text-sm"}>Preview</Text>
-                </TouchableOpacity>
+                <Text className={"font-semibold text-[16px] mb-5"}>Card {index}</Text>
+                <View className={"flex-row items-center gap-x-10"}>
+                    {
+                        // Preview icon
+                    }
+                    <TouchableOpacity className={"flex-row gap-x-1 hover:bg-gray-100 hover:rounded-md h-6 w-20 justify-center"}>
+                        <MaterialIcons name="visibility" size={18} color="black" />
+                        <Text className={"text-sm"}>Preview</Text>
+                    </TouchableOpacity>
+                    {
+                        // Delete icon
+                    }
+                    {canDelete && (
+                        <TouchableOpacity
+                            className={"hover:bg-gray-100 hover:rounded-md h-6 w-8 items-center"}
+                            onPress={onDelete}
+                        >
+                            <MaterialIcons name={"delete"} size={18} color={"red"} />
+                        </TouchableOpacity>
+                    )}
+                </View>
             </View>
             {
                 // Deck question
