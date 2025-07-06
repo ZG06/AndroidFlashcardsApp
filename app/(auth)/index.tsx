6d2003292773b785c252ef4a1cfd80fe9dbf5382
ZG06 <s1demusic06@gmail.com>
2025-07-07 00:11:11 +0200

    Add main authentication screen

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {Text, View, Image, TouchableOpacity, ScrollView, Platform} from "react-native";
+import {router} from "expo-router";
+
+
+export default function Index() {
+    return (
+        <ScrollView
+            style={{backgroundColor: '#E6EDFF'}}
+            contentContainerStyle={{flexGrow: 1}}
+        >
+            <View className={"flex-1 items-center justify-center"}>
+                <Image
+                    source={require('../../assets/icon.png')}
+                    style={{
+                        height: 128,
+                        width: 128
+                    }}
+                />
+                <Text className={"font-bold text-4xl mb-3"}>FlashMaster</Text>
+                <Text className={"text-gray-600 text-xl mb-8"}>Digital flashcards for learning</Text>
+
+                <TouchableOpacity
+                    className={"bg-black items-center justify-center rounded-md w-full max-w-[450px]"}
+                    style={{
+                        height: Platform.OS === 'web' ? 50 : 40,
+                        width: Platform.OS === 'web' ? 400 : 300
+                    }}
+                    onPress={() => router.push('/register')}
+                >
+                    <Text className={"text-white font-bold text-lg"}>Create Account</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    className={"bg-white items-center justify-center rounded-md w-full max-w-[450px]"}
+                    style={{
+                        height: Platform.OS === 'web' ? 50 : 40,
+                        width: Platform.OS === 'web' ? 400 : 300
+                    }}
+                    onPress={() => router.push('/login')}
+                >
+                    <Text className={"text-black font-bold text-lg"}>Sign In</Text>
+                </TouchableOpacity>
+                <Text className={"text-center text-gray-500 mt-8"}>
+                    By continuing, you agree to our Terms of Service and Privacy Policy
+                </Text>
+            </View>
+        </ScrollView>
+    );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,10 @@ export default function RootLayout() {
 
             <Stack>
                 <Stack.Screen
+                    name={'(auth)'}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
                     name={'(tabs)'}
                     options={{headerShown: false}}
                 />
