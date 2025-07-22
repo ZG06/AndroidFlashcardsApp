bf6d3a64ae8c289660fff3adaef2521dec03f477
ZG06 <s1demusic06@gmail.com>
2025-07-22 20:31:55 +0200

    Add logout functionality

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,13 +1,18 @@
-import {Text, View} from "react-native";
+import {Pressable, Text, View} from "react-native";
 import React from "react";
+import {useAuth} from "@/context/authContext";
 
 export default function Settings() {
+    const {logout} = useAuth();
+    const handleLogout = async () => {
+        await logout();
+    }
 
     return (
-        <View>
-            <Text>
-                Settings
-            </Text>
+        <View className={"items-center justify-center h-full"}>
+            <Pressable onPress={handleLogout}>
+                <Text>Sign Out</Text>
+            </Pressable>
         </View>
     );
 }
