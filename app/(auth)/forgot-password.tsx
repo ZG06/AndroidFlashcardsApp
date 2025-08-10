1d44fba698f2a1adb08a3a514fe47806a4901ad3
ZG06 <s1demusic06@gmail.com>
2025-08-10 15:52:30 +0200

    Add custom screen for password reset email confirmation

--- app/(auth)/forgot-password.tsx
+++ app/(auth)/forgot-password.tsx
@@ -1,19 +1,20 @@
-import React, {useState} from "react";
-import {Alert, Image, Platform, Text, TouchableOpacity, View} from "react-native";
-import AuthInput from "@/components/AuthInput";
-import {router} from "expo-router";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import {ErrorType} from "@/types/ErrorType";
-import AuthErrorBox from "@/components/AuthErrorBox";
-import KeyboardAvoidingContainer from "@/components/KeyboardAvoidingContainer";
 import ActivityIndicator from "@/components/ActivityIndicator";
-import {useAuth} from "@/context/authContext";
+import AuthErrorBox from "@/components/AuthErrorBox";
+import AuthInput from "@/components/AuthInput";
+import KeyboardAvoidingContainer from "@/components/KeyboardAvoidingContainer";
+import { useAuth } from "@/context/authContext";
+import { ErrorType } from "@/types/ErrorType";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { Image, Platform, ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 
 export default function ForgotPassword() {
     const [email, setEmail] = useState('');
     const [errorType, setErrorType] = useState<ErrorType>(null);
     const [isLoading, setIsLoading] = useState(false);
+    const [isSent, setIsSent] = useState(false);
 
     const {sendResetPasswordEmail} = useAuth();
 
@@ -33,10 +34,11 @@ export default function ForgotPassword() {
         const response = await sendResetPasswordEmail(email);
 
         if (response.success) {
-            // Display a success message to the user
-            Alert.alert("Success", "A password reset link has been sent to your email address.");
-            // Redirect the user back to the login screen
-            router.replace('/(auth)/login');
+            // // Display a success message to the user
+            // Alert.alert("Success", "A password reset link has been sent to your email address.");
+            // // Redirect the user back to the login screen
+            // router.replace('/(auth)/login');
+            setIsSent(true);
         } else {
             setErrorType(response.msg);
         }
@@ -44,7 +46,84 @@ export default function ForgotPassword() {
         setIsLoading(false);
     }
 
-    return (
+    return isSent ? (
+        <ScrollView
+            style={{backgroundColor: '#EBF2FF'}}
+            contentContainerStyle={{flexGrow: 1}}
+        >
+            <View className={"flex-1 items-center justify-center"}>
+                <Image
+                    source={require('../../assets/icon.png')}
+                    className={"mb-2"}
+                    style={{
+                        height: 72,
+                        width: 72
+                    }}
+                />
+                <Text className={"font-bold text-2xl mb-1"}>Check Your Email</Text>
+                <Text className={"text-center text-gray-600 text-[19px] mb-8"}>We&#39;ve sent a password reset link to {email}</Text>
+                <View
+                    className={"bg-white p-6 rounded-md shadow-md w-full mx-auto mt-2"}
+                    style={{
+                        maxWidth: Platform.OS === 'web' ? 420 : 320
+                    }}
+                >
+                    {
+
+                    }
+                    <View className={"items-center mb-3"}>
+                        {
+                            // Error box
+                        }
+                        {errorType && (
+                            <AuthErrorBox
+                                errorType={errorType}
+                            />
+                        )}
+                        <Text className={"text-center text-[16px] text-gray-600 mx-3"}>
+                            Didn&#39;t receive the email? Check your spam folder or try again.
+                        </Text>
+                    </View>
+
+                    {
+                        // Displays an activity indicator or a Resend Email button depending on the isLoading state
+                    }
+                    {isLoading ? (
+                        <View className={"items-center justify-center"}>
+                            <ActivityIndicator size={50} />
+                        </View>
+                    ) : (
+                        <TouchableOpacity
+                            className={"bg-white items-center justify-center rounded-md border-[1px] border-gray-200"}
+                            style={{
+                                height: Platform.OS === 'web' ? 40 : 40
+                            }}
+                            onPress={() => setIsSent(false)}
+                        >
+                            <Text className={"text-black font-medium text-[16px]"}>
+                                Try Different Email
+                            </Text>
+                        </TouchableOpacity>
+                    )}
+
+                    {
+                        // Back to sign in button
+                    }
+                    <TouchableOpacity
+                        className={"bg-black items-center justify-center rounded-md"}
+                        onPress={() => router.push('/login')}
+                        style={{
+                            height: Platform.OS === 'web' ? 40 : 40
+                        }}
+                    >
+                        <Text className={"text-white font-medium text-[16px]"}>
+                            Back to Sign In
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ScrollView>
+    ) : (
         <KeyboardAvoidingContainer>
             <View className={"flex-1 items-starts justify-center"}>
                 <View className={"flex-row items-center mx-auto"}>
