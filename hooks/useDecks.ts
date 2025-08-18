921d172d5cbfad1df91209836a043e739a4a6ff6
ZG06 <s1demusic06@gmail.com>
2025-08-18 15:05:34 +0200

    Move loading deck data function to a separate hook, and load decks on home screen

---
+++ hooks/useDecks.ts
@@ -0,0 +1,46 @@
+import { db } from "@/firebaseConfig";
+import { Deck } from "@/types/Deck";
+import { collection, FirestoreError, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+
+export const useDecks = (userId?: string) => {
+    const [decks, setDecks] = useState<Deck[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<FirestoreError | undefined>(undefined);
+
+    useEffect(() => {
+
+        if (!userId) {
+            setIsLoading(false);
+            setDecks([]);
+            return;
+        }
+
+        const q = query(collection(db, `users/${userId}/decks`), orderBy('createdAt', 'desc'));
+        setIsLoading(true);
+        const unsubscribe = onSnapshot(q, (snapshot => {
+            const mapped: Deck[] = [];
+            snapshot.forEach((doc) => {
+                const data = doc.data();
+                mapped.push({
+                    id: doc.id,
+                    name: data.name,
+                    description: data.description,
+                    cardsCount: data.cardsCount,
+                    createdAt: data.createdAt
+                });
+            });
+
+            setDecks(mapped);
+            setIsLoading(false);
+        }), error => {
+            setError(error);
+            setIsLoading(false);
+        });
+
+        return unsubscribe;
+    }, [userId])
+
+    return { decks, isLoading, error };
+}
