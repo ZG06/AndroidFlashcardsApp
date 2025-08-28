59bdd702c72ba708cd608cb3418a6325a1ac16d7
ZG06 <s1demusic06@gmail.com>
2025-08-28 19:39:51 +0200

    Add basic flashcard study system

--- app/(tabs)/decks/study/[deckId].tsx
+++ app/(tabs)/decks/study/[deckId].tsx
@@ -5,8 +5,8 @@ import { useDecks } from '@/hooks/useDecks';
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { router, useLocalSearchParams } from "expo-router";
 import { ChevronLeft, ChevronRight, RotateCcw } from 'lucide-react-native';
-import React, { useState } from "react";
-import { Platform, ScrollView, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Platform, ScrollView, TouchableOpacity, View } from "react-native";
 import * as Progress from 'react-native-progress';
 
 
@@ -15,16 +15,45 @@ export default function NewDeck() {
 
     const userId = auth.currentUser?.uid;
 
-    const { decks, isLoading, error: deckError } = useDecks('All', userId);
+    const { decks, isLoading: isDeckLoading, error: deckError } = useDecks('All', userId);
     const deck = decks.find((deck) => deck.id === deckId);
     const deckLength = deck?.cardsCount;
 
-    const { cards, error: cardsError } = useCards(deckId as string, userId);
-    
+    const { cards, isLoading: isCardLoading, error: cardsError } = useCards(deckId as string, userId);
 
-    const [isPreviewFront, setIsPreviewFront] = useState(true);
+    const [isAuthReady, setIsAuthReady] = useState(false);
+
+    const [isFront, setIsFront] = useState(true);
     const [frontValue, setFrontValue] = useState('');
     const [backValue, setBackValue] = useState('');
+    const [currentCardIndex, setCurrentCardIndex] = useState(0);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(() => {
+            setIsAuthReady(true);
+        });
+        return unsubscribe;
+    }, []);
+    
+    useEffect(() => {
+        if (cards.length > 0) {
+            setFrontValue(cards[currentCardIndex].front);
+            setBackValue(cards[currentCardIndex].back);
+            setIsFront(true);
+        }
+    }, [currentCardIndex, cards]); 
+
+    const isLoading = isDeckLoading || isCardLoading || !deckId || !userId || !deck;
+
+    if (isLoading) {
+        return (
+            <View className='flex-1 items-center justify-center'>
+                <ActivityIndicator size={50} />
+            </View> 
+        );
+    }
+
+    const progress = (currentCardIndex + 1) / deckLength!;
 
     return (
         <ScrollView
@@ -61,6 +90,14 @@ export default function NewDeck() {
                         {/* Reset button */}
                         <TouchableOpacity
                             className='flex-row items-center justify-center gap-x-[4px] border border-gray-200 rounded-md p-2.5 bg-white hover:bg-gray-100'
+                            disabled={(currentCardIndex === 0 && isFront)}
+                            style={[
+                                { opacity: (currentCardIndex === 0 && isFront) ? 0.4 : 1 }
+                            ]}
+                            onPress={() => {
+                                setCurrentCardIndex(0);
+                                setIsFront(true);
+                            }}
                         >
                             <RotateCcw color={"black"} size={16} />
                             <Text weight='medium'>
@@ -72,29 +109,32 @@ export default function NewDeck() {
                 <View className={"mt-6"}>
                     <View className={"flex-row justify-between mb-3"}>
                         <Text className='text-gray-600 text-[15px]'>
-                            Card 0 of {deckLength}
+                            Card {currentCardIndex + 1} of {deckLength}
                         </Text>
                         <Text className='text-gray-600 text-[15px]'>
-                            0 studied
+                            {currentCardIndex + 1} studied
                         </Text>
                     </View>
                     <View className={"w-full"}>
-                        <Progress.Bar progress={(0/deckLength!)} color={"black"} width={null} borderColor={"white"} unfilledColor={"#f3f4f6"} height={8} />
+                        <Progress.Bar progress={progress} color={"black"} width={null} borderColor={"white"} unfilledColor={"#f3f4f6"} height={8} />
                     </View>
                 </View>
                 <View className='mt-6'>
-                    {isPreviewFront ? (
+                    
+
+                    {isFront ? (
+
                         // Front side preview
                         <TouchableOpacity
                             className={"border-[1px] rounded-md border-gray-300 h-[350px] bg-white"}
-                            onPress={() => setIsPreviewFront(false)}
+                            onPress={() => setIsFront(false)}
                         >
                             <View className={"flex-1 items-center justify-center gap-y-5"}>
                                 <Text className={"text-center text-sm text-gray-500"}>
                                     QUESTION
                                 </Text>
-                                <Text weight="semibold" className={"text-center text-gray-900 text-lgo"}>
-                                    { frontValue || 'Front side content...'}
+                                <Text weight="medium" className={"text-center text-gray-900 text-xl"}>
+                                    {frontValue}
                                 </Text>
                             </View>
                             <View>
@@ -107,14 +147,14 @@ export default function NewDeck() {
                         // Back side preview
                         <TouchableOpacity
                             className={"border-[1px] rounded-md border-gray-300 h-[350px] bg-white"}
-                            onPress={() => setIsPreviewFront(true)}
+                            onPress={() => setIsFront(true)}
                         >
                             <View className={"flex-1 items-center justify-center gap-y-5 p-6"}>
                                 <Text className={"text-center text-sm text-gray-500"}>
                                     ANSWER
                                 </Text>
-                                <Text weight="semibold" className={"text-center text-gray-900 text-lgo"}>
-                                { backValue || 'Back side content...'}
+                                <Text weight="medium" className={"text-center text-gray-900 text-xl"}>
+                                {backValue}
                                 </Text>
                             </View>
                             <View>
@@ -129,6 +169,12 @@ export default function NewDeck() {
                         {/* Previous button */}
                         <TouchableOpacity
                             className='flex-row items-center justify-center gap-x-[4px] border border-gray-200 rounded-md p-2.5 bg-white hover:bg-gray-100'
+                            // Disable the 'Previous' button on the first card in the decks
+                            disabled={currentCardIndex === 0}
+                            style={[
+                                { opacity: (currentCardIndex === 0) ? 0.4 : 1 }
+                            ]}
+                            onPress={() => setCurrentCardIndex(prev => prev - 1)}
                         >
                             <ChevronLeft size={16} color={'black'} />
                             <Text weight='medium'>
@@ -139,6 +185,12 @@ export default function NewDeck() {
                         {/* Next button */}
                         <TouchableOpacity
                             className='flex-row items-center justify-center gap-x-[4px] border border-gray-200 rounded-md p-2.5 bg-white hover:bg-gray-100'
+                            // Disable the 'Next' button on the last card in the decks
+                            disabled={currentCardIndex === (deckLength! - 1)}
+                            style={[
+                                { opacity: (currentCardIndex === (deckLength! - 1)) ? 0.4 : 1 }
+                            ]}
+                            onPress={() => setCurrentCardIndex(prev => prev + 1)}
                         >
                             <Text weight='medium'>
                                 Next
--- hooks/useCards.ts
+++ hooks/useCards.ts
@@ -5,33 +5,35 @@ import { useEffect, useState } from "react";
 
 export const useCards = (deckId: string, userId?: string) => {
     const [cards, setCards] = useState<FlashCard[]>([]);
+    const [isLoading, setIsLoading] = useState(true);   
     const [error, setError] = useState<FirestoreError | undefined>(undefined);
 
     useEffect(() => {
-        if (!userId) {
+        if (!userId || !deckId) {
+            setIsLoading(false);
             setCards([]);
             return;
         }
 
+        setIsLoading(true);
         const q = query(collection(db, `users/${userId}/decks/${deckId}/cards`), orderBy('createdAt', 'desc'));
-        const unsubscribe = onSnapshot(q, (snapshot => {
-            const mapped: FlashCard[] = [];
-            snapshot.forEach((doc) => {
-                const data = doc.data();
-                mapped.push({
-                    id: doc.id,
-                    front: data.front,
-                    back: data.back
-                });
-            });
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const mapped: FlashCard[] = snapshot.docs.map(doc => ({
+                id: doc.id, 
+                front: doc.data().front,
+                back: doc.data().back
+            }));
             
             setCards(mapped);
-        }), error => {
+            setIsLoading(false);
+        }, 
+        (error) => {
             setError(error);
+            setIsLoading(false);
         });
 
-        return unsubscribe;
+        return () => unsubscribe();
     }, [deckId, userId])
 
-    return { cards, error };
+    return { cards, isLoading, error };
 }
