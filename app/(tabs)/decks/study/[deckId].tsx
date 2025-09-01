78d1c4cabb5fe28ff6bd5bbb27f85429b8ca46e9
ZG06 <s1demusic06@gmail.com>
2025-09-01 21:57:34 +0200

    Implement card difficulty tracking in study mode
    - Add easy/hard card tracking with accuracy calculation
    - Implement track progress toggle with visual feedback
    - Add back navigation in track mode

--- app/(tabs)/decks/study/[deckId].tsx
+++ app/(tabs)/decks/study/[deckId].tsx
@@ -4,9 +4,9 @@ import { useCards } from '@/hooks/useCards';
 import { useDecks } from '@/hooks/useDecks';
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { router, useLocalSearchParams } from "expo-router";
-import { ChevronLeft, ChevronRight, RotateCcw } from 'lucide-react-native';
+import { Check, ChevronLeft, ChevronRight, RotateCcw, X } from 'lucide-react-native';
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, Platform, ScrollView, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Platform, ScrollView, Switch, TouchableOpacity, View } from "react-native";
 import * as Progress from 'react-native-progress';
 
 
@@ -23,10 +23,51 @@ export default function NewDeck() {
 
     const [isAuthReady, setIsAuthReady] = useState(false);
 
+    const [isTrackProgressMode, setIsTrackProgressMode] = useState(false);
     const [isFront, setIsFront] = useState(true);
     const [frontValue, setFrontValue] = useState('');
     const [backValue, setBackValue] = useState('');
     const [currentCardIndex, setCurrentCardIndex] = useState(0);
+    const [learnedCards, setLearnedCards] = useState<{ cardId: string; difficulty: 'easy' | 'hard' | null }[]>([]);
+
+    const easyCards = learnedCards.filter((card) => card.difficulty === 'easy').length;
+    const hardCards = learnedCards.filter((card) => card.difficulty === 'hard').length;
+    const cardAccuracy = (easyCards / (easyCards + hardCards) * 100) | 0;
+    const currentCardDifficulty = learnedCards.find((card) => card.cardId === cards[currentCardIndex].id)?.difficulty;
+
+    const handleSelectDifficulty = (difficulty: 'easy' | 'hard') => {
+        setLearnedCards(prev => {
+            const updated = [...prev];
+            const currentCard = cards[currentCardIndex];
+            const isIncluded = updated.find((card) => card.cardId === currentCard.id)
+
+            if (!isIncluded) {
+                updated.push({
+                    cardId: cards[currentCardIndex].id,
+                    difficulty: difficulty
+                });
+            } else {
+                updated.find((card) => card.cardId === currentCard.id)!.difficulty = difficulty;
+            }
+
+            return updated
+        });
+
+        if (currentCardIndex !== cards.length - 1) {
+            setCurrentCardIndex(prev => prev + 1);
+        }
+    };
+
+    const handleTrackModeBack = () => {
+
+        setLearnedCards(prev => 
+            prev.filter(card =>
+                card.cardId !== cards[currentCardIndex].id &&
+                card.cardId !== cards[currentCardIndex - 1].id
+            )
+        );
+        setCurrentCardIndex(prev => prev - 1);
+    }
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(() => {
@@ -61,7 +102,7 @@ export default function NewDeck() {
             contentContainerStyle={{flexGrow: 1}}
         >
             <View
-                className={"flex-1 justify-center w-full mx-auto"}
+                className={"flex-1 justify-center w-full mx-auto mt-6"}
                 style={{
                     maxWidth: Platform.OS === 'web' ? 500 : 370
                 }}
@@ -89,10 +130,10 @@ export default function NewDeck() {
 
                         {/* Reset button */}
                         <TouchableOpacity
-                            className='flex-row items-center justify-center gap-x-[4px] border border-gray-200 rounded-md p-2.5 bg-white hover:bg-gray-100'
-                            disabled={(currentCardIndex === 0 && isFront)}
+                            className={`flex-row items-center justify-center gap-x-[4px] border border-gray-200 rounded-md p-2.5 bg-white ${currentCardIndex === 0 ? '' : 'hover:bg-gray-100'} `}
+                            disabled={(currentCardIndex === 0)}
                             style={[
-                                { opacity: (currentCardIndex === 0 && isFront) ? 0.4 : 1 }
+                                { opacity: (currentCardIndex === 0) ? 0.4 : 1 }
                             ]}
                             onPress={() => {
                                 setCurrentCardIndex(0);
@@ -108,22 +149,52 @@ export default function NewDeck() {
                 </View>
                 <View className={"mt-6"}>
                     <View className={"flex-row justify-between mb-3"}>
+                        {/* Current card number */}
                         <Text className='text-gray-600 text-[15px]'>
                             Card {currentCardIndex + 1} of {deckLength}
                         </Text>
+                        {/* Number of cards studies (selected as easy or hard) */}
                         <Text className='text-gray-600 text-[15px]'>
-                            {currentCardIndex + 1} studied
+                            0 studied
                         </Text>
                     </View>
-                    <View className={"w-full"}>
+                    {/* Progres bar with number of cards reviewed */}
+                    <View className={"w-full mb-3"}>
                         <Progress.Bar progress={progress} color={"black"} width={null} borderColor={"white"} unfilledColor={"#f3f4f6"} height={8} />
                     </View>
-                </View>
-                <View className='mt-6'>
+
+                    {/* Only show statistics if track progress mode is active */}
+                    {isTrackProgressMode && (
+                        <View className='flex-row items-center justify-between'>
+                            {/* Number of cards selected as 'easy' */}
+                            <View className='flex-row items-center justify-center gap-x-1'>
+                                <Check size={13} color="#15A349" />
+                                <Text className='text-green-600 text-[14px]'>
+                                    {easyCards}
+                                </Text>
+                            </View>
+
+                            {/* Number of cards selected as 'hard' */}
+                            <View className='flex-row items-center justify-center gap-x-1'>
+                                <X size={13} color="#DB2525" />
+                                <Text className='text-red-600 text-[14px]'>
+                                    {hardCards}
+                                </Text>
+                            </View>
+
+                            {/* Accuracy of answers (how many % of cards were selected as 'easy') */}
+                            <View className='flex-row items-center justify-center gap-x-1'>
+                                <Text className='text-gray-500 text-[14px]'>
+                                    {cardAccuracy}% accuracy
+                                </Text>
+                            </View>
+                        </View>
+                    )}
                     
+                </View>
+                <View className='flex-1 mt-6'>
 
                     {isFront ? (
-
                         // Front side preview
                         <TouchableOpacity
                             className={"border-[1px] rounded-md border-gray-300 h-[350px] bg-white"}
@@ -145,58 +216,126 @@ export default function NewDeck() {
                         </TouchableOpacity>
                     ) : (
                         // Back side preview
-                        <TouchableOpacity
-                            className={"border-[1px] rounded-md border-gray-300 h-[350px] bg-white"}
-                            onPress={() => setIsFront(true)}
-                        >
-                            <View className={"flex-1 items-center justify-center gap-y-5 p-6"}>
-                                <Text className={"text-center text-sm text-gray-500"}>
-                                    ANSWER
-                                </Text>
-                                <Text weight="medium" className={"text-center text-gray-900 text-xl"}>
-                                {backValue}
-                                </Text>
-                            </View>
-                            <View>
-                                <Text className={"text-gray-400 text-xs text-center mb-4"}>
-                                    Tap to flip back
-                                </Text>
-                            </View>
-                        </TouchableOpacity>
+                        <>
+                             {/* Back side of the card */}
+                            <TouchableOpacity
+                                className={"border-[1px] rounded-md border-gray-300 h-[350px] bg-white"}
+                                onPress={() => setIsFront(true)}
+                            >
+                                <View className={"flex-1 items-center justify-center gap-y-5 p-6"}>
+                                    <Text className={"text-center text-sm text-gray-500"}>
+                                        ANSWER
+                                    </Text>
+                                    <Text weight="medium" className={"text-center text-gray-900 text-xl"}>
+                                    {backValue}
+                                    </Text>
+                                </View>
+                                <View>
+                                    <Text className={"text-gray-400 text-xs text-center mb-4"}>
+                                        Tap to flip back
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                        </>
                     )}
+
                     {/* Navigation buttons */}
                     <View className='flex-row items-center justify-between mt-6'>
-                        {/* Previous button */}
-                        <TouchableOpacity
-                            className='flex-row items-center justify-center gap-x-[4px] border border-gray-200 rounded-md p-2.5 bg-white hover:bg-gray-100'
-                            // Disable the 'Previous' button on the first card in the decks
-                            disabled={currentCardIndex === 0}
-                            style={[
-                                { opacity: (currentCardIndex === 0) ? 0.4 : 1 }
-                            ]}
-                            onPress={() => setCurrentCardIndex(prev => prev - 1)}
-                        >
-                            <ChevronLeft size={16} color={'black'} />
-                            <Text weight='medium'>
-                                Previous
-                            </Text>
-                        </TouchableOpacity>
 
-                        {/* Next button */}
-                        <TouchableOpacity
-                            className='flex-row items-center justify-center gap-x-[4px] border border-gray-200 rounded-md p-2.5 bg-white hover:bg-gray-100'
-                            // Disable the 'Next' button on the last card in the decks
-                            disabled={currentCardIndex === (deckLength! - 1)}
-                            style={[
-                                { opacity: (currentCardIndex === (deckLength! - 1)) ? 0.4 : 1 }
-                            ]}
-                            onPress={() => setCurrentCardIndex(prev => prev + 1)}
-                        >
-                            <Text weight='medium'>
-                                Next
+                        {/* If track mode is active, show 'hard' button, else - show 'previous' button */}
+                        {!isTrackProgressMode ? (
+                            // Previous button
+                            <TouchableOpacity
+                                className={`flex-row items-center justify-center gap-x-[4px] border border-gray-200 rounded-md p-2.5 bg-white ${currentCardIndex === 0 ? '' : 'hover:bg-gray-100'}`}
+                                // Disable the 'Previous' button on the first card in the decks
+                                disabled={currentCardIndex === 0}
+                                style={[
+                                    { opacity: (currentCardIndex === 0) ? 0.4 : 1 }
+                                ]}
+                                onPress={handleTrackModeBack}
+                            >
+                                <ChevronLeft size={16} color={'black'} />
+                                <Text weight='medium'>
+                                    Previous
+                                </Text>
+                            </TouchableOpacity>
+                        ) : (
+                            // Hard button
+                            <TouchableOpacity
+                                className={`flex-row items-center justify-center p-2.5 border border-red-200 rounded-md h-10 bg-red-50 hover:opacity-70`}
+                                // Set current card difficulty as 'hard'
+                                onPress={() => handleSelectDifficulty('hard')}
+                            >
+                                <View
+                                    className='flex-row items-center justify-center gap-x-1.5'
+                                >
+                                    <X size={16} color="#DB2525" />
+                                    <Text weight='medium' className='text-red-700 -mt-0.5'>
+                                        Hard
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                        )}
+
+                        
+                        <View className='flex-row items-center justify-center gap-x-[4px]'>
+                            {isTrackProgressMode && (
+                                <TouchableOpacity
+                                    disabled={currentCardIndex === 0}
+                                    onPress={handleTrackModeBack}
+                                    style={[
+                                        { opacity: (currentCardIndex === 0) ? 0.4 : 1 }
+                                    ]}
+                                >
+                                    <ChevronLeft size={22} color={currentCardIndex === 0 ? "#9ca3af" : "#4b5563"} />
+                                </TouchableOpacity>
+                            )}
+                            <Text weight='medium' className=''>
+                                Track progress
                             </Text>
-                            <ChevronRight size={16} color={'black'} />
-                        </TouchableOpacity>
+                            <Switch
+                                value={isTrackProgressMode}
+                                onValueChange={setIsTrackProgressMode}
+                                trackColor={{ 
+                                    false: "#e5e7eb",
+                                    true: "#0d9488"
+                                }}
+                                thumbColor={"#ffffff"}
+                            />
+                        </View>
+
+                        {/* If track mode is active, show 'easy' button, else - show 'next' button */}
+                        {!isTrackProgressMode ? (
+                            // Next button
+                            <TouchableOpacity
+                                className={`flex-row items-center justify-center gap-x-[4px] border border-gray-200 rounded-md p-2.5 bg-white ${currentCardIndex === (deckLength! - 1) ? '' : 'hover:bg-gray-100'}`}
+                                // Disable the 'Next' button on the last card in the decks
+                                disabled={currentCardIndex === (deckLength! - 1)}
+                                style={[
+                                    { opacity: (currentCardIndex === (deckLength! - 1)) ? 0.4 : 1 }
+                                ]}
+                                onPress={() => handleSelectDifficulty('easy')}
+                            >
+                                <Text weight='medium'>
+                                    Next
+                                </Text>
+                                <ChevronRight size={16} color={'black'} />
+                            </TouchableOpacity>
+                        ) : (
+                            // Easy button
+                            <TouchableOpacity
+                                className={`items-center justify-center rounded-md p-2.5 bg-green-600 h-10 hover:opacity-70`}
+                                // Set current card difficulty as 'easy'
+                                onPress={() => handleSelectDifficulty('easy')}
+                            >
+                                <View className='flex-row items-center justify-center gap-x-1.5'>
+                                    <Check size={16} color="white" />
+                                    <Text weight='medium' className='text-white -mt-0.5'>
+                                        Easy
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                        )}
                     </View>
                 </View>
             </View>
